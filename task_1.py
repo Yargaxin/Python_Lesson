c90dc303abdbdb9c7d22fda6183d77750386eb3c
Yaroslav Romanov <67858515+Yargaxin@users.noreply.github.com>
2020-09-25 15:05:10 +0300

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,13 +1,15 @@
 def file():
+	my_text = None
 	try:
-		with open('my_file_1.txt', 'w+', encoding='utf-8') as my_file:
-			my_text = input('Enter text: ')
-			my_file.write(my_text)
-			my_file.seek(0)
-			read_file = my_file.read()
-			print(f'\nFile content: {read_file}')
-			print(f'Number of words in file: {len(read_file.split())}')
+		while True:
+			with open('my_file.txt', 'w', encoding='utf-8') as my_file:
+				my_text = input('Enter text: ')
+				my_file.write(my_text)
+				yes_or_no = input('Want to overwrite a file (y/n):').lower()
+				if yes_or_no == 'n':
+					break
+				else:
+					pass
 	except:
 		print('Error!')
-
 file()
