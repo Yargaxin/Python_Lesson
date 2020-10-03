4ffda03ab480e0a44e1c8e4456aff98f533de389
Yaroslav Romanov <67858515+Yargaxin@users.noreply.github.com>
2020-10-03 12:32:13 +0300

    Update task_1.py

--- lesson_7/task_1.py
+++ lesson_7/task_1.py
@@ -1,9 +1,9 @@
 from random import randint
 rand_m1_left = [randint(121, 590) for i in range(2)]
 rand_m1_middle = [randint(45, 100) for i in range(2)]
-rand_m1_right = [randint(100, 200) for i in range(2)]
-rand_m2_left = [randint(-56, 5) for i in range(2)]
-rand_m2_middle = [randint(-360, 700) for i in range(2)]
+rand_m1_right = [randint(100, 230) for i in range(2)]
+rand_m2_left = [randint(100, 255) for i in range(2)]
+rand_m2_middle = [randint(360, 700) for i in range(2)]
 rand_m2_right = [randint(253, 870) for i in range(2)]
 
 class Matrix():
@@ -18,7 +18,7 @@ class Matrix():
 		for i in range(len(self.matrix)):
 			for j in range(len(other.matrix[i])):
 				new_matrix[i][j] = self.matrix[i][j] + other.matrix[i][j]
-		return str('\n'.join(['\t '.join([str(j) for j in i]) for i in new_matrix]))
+		return str('\n'.join(['\t'.join([str(j) for j in i]) for i in new_matrix]))
 	
 	def __str__(self):
 		return f'{self.matrix}\n'
