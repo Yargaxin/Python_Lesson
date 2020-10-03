92df1aca2af2d880b661f5236fbed2b2fe81198a
Yaroslav Romanov <67858515+Yargaxin@users.noreply.github.com>
2020-10-03 12:42:08 +0300

    Update task_3.py

--- lesson_7/task_3.py
+++ lesson_7/task_3.py
@@ -1,49 +1,34 @@
-s = '\N{Winking Face}'
-
 class Cell():
 	def __init__(self, amount):
 		self.amount = amount
 
+	def __str__(self):
+		return self.amount
+
 	def __add__(self, other):
-		return Cell(self.amount + other.amount)
+		return self.amount + other.amount
 
 	def __sub__(self, other):
 		if self.amount < other.amount:
 			print('Negative!')
 			return 'Error!'
 		else:
-			return Cell(self.amount - other.amount)
+			return self.amount - other.amount
 
 	def __mul__(self, other):
-		return Cell(self.amount * other.amount)
+		return self.amount * other.amount
 
 	def __truediv__(self, other):
-		return Cell(round(self.amount / other.amount))
-
-	#Add
-	def __str__(self):
-		return f'{self.amount}'
-	#Sub
-	def __str__(self):
-		return f'{self.amount}'
-
-	#Mul
-	def __str__(self):
-		return f'{self.amount}'
-
-	#Floordiv
-	def __str__(self):
-		return f'{self.amount}'
+		return round(self.amount / other.amount)
 
 	def make_order(self, rows):
-		cells = (rows) * (f'{s} ') * round(self.amount / rows)
-		return cells
+		return (rows * '*' + '\n') * round(self.amount / rows)
 
-cell_1 = Cell(6)
-cell_2 = Cell(6)
+cell_1 = Cell(30)
+cell_2 = Cell(24)
 cell_3 = cell_1 + cell_2
 print(f'Add: {cell_3}')
-print(cell_3.make_order(12))
-print(f'Sub: {cell_1.__sub__(cell_2)}')
-print(f'Mul: {cell_1.__mul__(cell_2)}')
-print(f'Div: {cell_1.__truediv__(cell_2)}')
+print(f'Sub: {cell_1 - cell_2}')
+print(f'Mul: {cell_1 * cell_2}')
+print(f'Div: {cell_1 / cell_2}')
+print(cell_2.make_order(7))
