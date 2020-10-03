b1f39d68f68fad326e65fee4b2bfe9be4401dc85
Yaroslav Romanov <67858515+Yargaxin@users.noreply.github.com>
2020-10-03 12:48:49 +0300

    Update task_2.py

--- lesson_7/task_2.py
+++ lesson_7/task_2.py
@@ -1,29 +1,29 @@
-size = float(input('Enter coat size: '))
-height = float(input('Enter your height (meter): '))
+from abc import ABC, abstractmethod
 
-class Clothing():
-	def __init__(self, size, height):
+class Clothing(ABC):
+	def __init__(self, size):
 		self.size = size
-		self.height = height
-		self.coat_tissue = round(self.size / 6.5 + 0.5, 2)
-		self.suit_tissue = round(2 * self.height + 0.3, 2)
-		self.all_profit = round(self.coat_tissue + self.suit_tissue, 2)
 
-	def all_prof(self):
-		return f'Total tissue consumption: {self.all_profit} meters'
+	@property
+	@abstractmethod
+	def formula(self):
+		pass
+
+	def __add__(self, other):
+		return f'Total fabric: {self.formula + other.formula} metrs'
 
 class Coat(Clothing):
 	@property
-	def coat_formula(self):
-		return f'How much fabric does a coat need: {self.coat_tissue}'
+	def formula(self):
+		print(f'How much fabric does a coat need: {round(self.size / 6.5 + 0.5, 2)}')
+		return round(self.size / 6.5 + 0.5, 2)
 
 class Suit(Clothing):
-	def suit_formula(self):
-		return f'How much fabric does a suit need: {self.suit_tissue}\n'
+	@property
+	def formula(self):
+		print(f'How much fabric does a suit need: {round(2 * self.size + 0.3, 2)}')
+		return round(2 * self.size + 0.3, 2)
 
-clothing = Clothing(size, height)
-coat = Coat(size, height)
-suit = Suit(size, height)
-print(coat.coat_formula)
-print(suit.suit_formula())
-print(clothing.all_prof())
+coat = Coat(10)
+suit = Suit(3)
+print(coat + suit)
